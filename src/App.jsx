9fef6c37526aff75056b586d57a806ed92c41138
Ashish kumar <ashishkumar082018@gmail.com>
2024-06-30 23:02:10 +0530

    Created Basic React Project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import Lottery from "./Lottery"
+import Ticket from "./Ticket"
+import { sum } from "./helper"
+
+function App() {
+
+  let winCondition = (ticket) => {
+    return sum(ticket) === 15
+  }
+
+
+  return <>
+
+    <Lottery n={3} winCondition={winCondition} />
+
+  </>
+}
+
+export default App
